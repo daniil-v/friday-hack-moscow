497a6a1475924ec8ed67a4b8d690f340069ea872
dv <dvodolazkin@gmail.com>
2019-10-26 23:57:02 +0300

    some ui

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-import { MenuItem } from '@material-ui/core';
+import { MenuItem, Grid } from '@material-ui/core';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-
 import AccountCircle from '@material-ui/icons/AccountCircle';
-
 import Menu from '@material-ui/core/Menu';
+
+import { teal, blue } from '@material-ui/core/colors/';
 
 import { makeStyles } from '@material-ui/core/styles';
 
 import ActionList from './ActionList';
 import Messages from './Messages';
 
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
+import ViewListIcon from '@material-ui/icons/ViewList';
+import TextsmsIcon from '@material-ui/icons/Textsms';
+
 const useStyles = makeStyles(theme => ({
   root: {
     textAlign: 'left',
     backgroundColor: theme.palette.grey[100],
+  },
+  appBar: {
+    zIndex: 9999,
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -30,20 +36,40 @@ const useStyles = makeStyles(theme => ({
   title: {
     flexGrow: 1,
   },
+  sideBar: {
+      height: '100%',
+      width: theme.spacing(8), 
+      position: 'fixed',
+      top: 64,
+      left: 0,
+      backgroundColor: teal[200],
+      padding: 6
+  },
+  iconSmall: {
+    fontSize: 30,
+    color: blue[100],
+  },
+  active: {
+    fontSize: 30,
+    color: 'white',
+  }
 }));
 
 const App = () => {
   const classes = useStyles();
   const auth = true;
+
+  const [ page, setPage ] = useState('Messages');
+
   return (
     <div className={`App ${classes.root}`}>
-       <AppBar position="static">
+       <AppBar position="static" className={classes.appBar}>
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            SVYATOGOR
+            FRIDAY
           </Typography>
           {auth && (
             <div>
@@ -58,7 +84,6 @@ const App = () => {
               </IconButton>
               <Menu
                 id="menu-appbar"
-                // anchorEl={anchorEl}
                 anchorOrigin={{
                   vertical: 'top',
                   horizontal: 'right',
@@ -78,9 +103,25 @@ const App = () => {
           )}
         </Toolbar>
       </AppBar>
-      {/* <ActionList /> */}
-      <Messages />
+      <Grid container direction="row" justify="flex-start" alignItems="flex-start" >
+        <Grid item className={classes.sideBar}>
+          <IconButton onClick={() => setPage('Messages')} >
+            <TextsmsIcon className={page === 'Messages' ? classes.active : classes.iconSmall}/>
+          </IconButton>
+          <IconButton onClick={() => setPage('ActionList')} >
+            <ViewListIcon className={page === 'ActionList' ? classes.active : classes.iconSmall}/>
+          </IconButton>
+          <IconButton >
+            <CalendarTodayIcon className={classes.iconSmall}/>
+          </IconButton>
+        </Grid>
+        <Grid container item direction="row" justify="center" alignItems="center" >
+          {page === 'ActionList' && <ActionList />}
+          {page === 'Messages' && <Messages />}
+        </Grid>
+      </Grid>
     </div>
+
   );
 }
 
--- src/Messages.js
+++ src/Messages.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
-import { Paper, Grid, Divider } from '@material-ui/core';
+import { Paper, Grid, Divider, LinearProgress } from '@material-ui/core';
 import { blueGrey, blue } from '@material-ui/core/colors/';
 
 import AccountCircle from '@material-ui/icons/AccountCircle';
+
 import ChatIcon from '@material-ui/icons/Chat';
 import CallIcon from '@material-ui/icons/Call';
 import { makeStyles } from '@material-ui/core/styles';
@@ -18,6 +19,10 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(2),
     backgroundColor: 'white',
     overflowY: 'scroll',
+  },
+  header: {
+    fontWeight: 800,
+    marginBottom: theme.spacing(2),
   },
   block: {
     padding: theme.spacing(1),
@@ -45,13 +50,15 @@ const Messages = () => {
   }, []);
 
   const getMessages = () => {
-    axious.get(`http://84.201.146.49:8000/messages/`)
+    axious.get(`http://84.201.146.49:8000/messages/1`)
       .then(({ data }) => { setMessages(data) });
   }
-
+// upcoming appointments
   return (
     <Grid container direction="row" justify="center" alignItems="flex-start" >
       <Paper elevation={2} className={classes.wrapper}>
+        <div className={classes.header}>Upcoming appointments</div>
+        {(!messages || !messages.length) && <LinearProgress />}
         <Grid container spacing={2}>
           {messages && messages.map(message => (
             <Grid container spacing={4} item xs={12} key={`message_${message.id}`}>
@@ -70,7 +77,9 @@ const Messages = () => {
                       </Grid>
                       <Divider component="div" />
                       <Grid item xs={12} style={{paddingTop: 8}}>
-                        {message.spelled_out ? message.action : 'We did not manage to split out this request'}
+                        {message.spelled_out 
+                          ? message.action 
+                          : <span style={{color: 'red'}}>We did not manage to split out this request</span>}
                       </Grid>
                       <span style={{fontSize: 12}}>Original text: {message.message_text}</span>
                     </Grid>
